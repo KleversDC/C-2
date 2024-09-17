e56ca227e2f9fdbb66546e573651cf16376dd90f
KleversDS <klevers170@gmail.com>
2024-09-17 13:29:12 +0300

    Добавьте файлы проекта.

---
+++ C+++2/C+++2.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+using namespace std;
+//int main() {
+//    const int size = 10;
+//    int arr[size];
+//    srand(time(0));
+//    cout << "Масив: ";
+//    for (int i = 0; i < size; i++) {
+//        arr[i] = rand() % 100;
+//        cout << arr[i] << " ";
+//    }
+//    cout << endl;
+//    int min = arr[0];
+//    int max = arr[0];
+//    for (int i = 1; i < size; i++) {
+//        if (arr[i] < min) {
+//            min = arr[i];
+//        }
+//        if (arr[i] > max) {
+//            max = arr[i];
+//        }
+//    }
+//    cout << "Minimal element: " << min << endl;
+//    cout << "Maximum element: " << max << endl;
+//}
+
+//int main() {
+//    const int months = 12;
+//    double profit[months];
+//
+//    cout << "Enter the profit for each month:" << endl;
+//    for (int i = 0; i < months; i++) {
+//        cout << "Місяць " << i + 1 << ": ";
+//        cin >> profit[i];
+//    }
+//    int start, end;
+//    cout << "Enter the month range: ";
+//    cin >> start >> end;
+//    if (start < 1 || end > 12 || start > end) {
+//    }
+//    double min_profit = profit[start - 1];
+//    double max_profit = profit[start - 1];
+//    int min_month = start;
+//    int max_month = start;
+//    for (int i = start; i <= end; i++) {
+//        if (profit[i - 1] < min_profit) {
+//            min_profit = profit[i - 1];
+//            min_month = i;
+//        }
+//        if (profit[i - 1] > max_profit) {
+//            max_profit = profit[i - 1];
+//            max_month = i;
+//        }
+//    }
+//    cout << "A month with minimal profit: " << min_month << " Profit: " << min_profit << ")" << endl;
+//    cout << "The month with maximum profit: " << max_month << " Profit: " << max_profit << ")" << endl;
+}
+
+
+
